c239f290a13d807d943318303f3fe27302e2e713
Mengyi Wang <mengyi.wang@canonical.com>
2023-01-26 09:54:20 +0100

    Add run-go-tests

--- data/latest/run-all-tests-locally.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-rm -rf edgex-snap-testing
-
-git clone https://github.com/canonical/edgex-snap-testing.git
-cd edgex-snap-testing
-PLATFORM_CHANNEL="latest/beta" FULL_CONFIG_TEST=true go test -failfast -p 1 -timeout 30m -v ./test/suites/edgexfoundry
-
-rm -rf edgex-snap-testing
