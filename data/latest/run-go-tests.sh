d2050e5578e31ef1e417d25fdb1c88f9a3a1e4c7
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2023-02-15 17:31:33 +0100

    Set to print commands to improved debugging

--- data/latest/run-go-tests.sh
+++ data/latest/run-go-tests.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 # This scripts runs the EdgeX Snap tests maintained in
 # https://github.com/canonical/edgex-snap-testing
@@ -6,28 +6,21 @@
 # Arguments:
 SUITE=$1 # name of the Go testing package
 
-# Default to beta risk level of this track
-TRACK_BETA=latest/beta
-if [ -n "$DEFAULT_TEST_CHANNEL" ]; then
-    echo "DEFAULT_TEST_CHANNEL set to $DEFAULT_TEST_CHANNEL"
-else
-    echo "DEFAULT_TEST_CHANNEL not set. Setting to $TRACK_BETA"
-    export DEFAULT_TEST_CHANNEL=$TRACK_BETA
-fi
+DEFAULT_TEST_CHANNEL=${DEFAULT_TEST_CHANNEL:-latest/beta}
 
 # Map input variables to those expected by the Go tests
-export PLATFORM_CHANNEL=$DEFAULT_TEST_CHANNEL
-export SERVICE_CHANNEL=$DEFAULT_TEST_CHANNEL
+PLATFORM_CHANNEL=$DEFAULT_TEST_CHANNEL
+SERVICE_CHANNEL=$DEFAULT_TEST_CHANNEL
 
-# Setup the environment
-export GIT_EXEC_PATH=$SNAP/usr/lib/git-core
-export GIT_TEMPLATE_DIR=$SNAP/usr/share/git-core/templates
-export GIT_CONFIG_NOSYSTEM=1
-export PATH=$PATH:$SNAP/usr/lib/go-1.18/bin
-export CGO_ENABLED=0
+GIT_EXEC_PATH=$SNAP/usr/lib/git-core
+GIT_TEMPLATE_DIR=$SNAP/usr/share/git-core/templates
+GIT_CONFIG_NOSYSTEM=1
+PATH=$PATH:$SNAP/usr/lib/go-1.18/bin
+CGO_ENABLED=0
+
+export PLATFORM_CHANNEL SERVICE_CHANNEL GIT_EXEC_PATH GIT_TEMPLATE_DIR GIT_CONFIG_NOSYSTEM PATH CGO_ENABLED
 
 rm -rf tmp
-
 git clone --config advice.detachedHead=false --depth 1 --branch v3 \
     https://github.com/canonical/edgex-snap-testing.git tmp/edgex-snap-testing
 cd tmp/edgex-snap-testing
@@ -48,5 +41,7 @@ trap print_logs EXIT
 sed -i '/TestTLSCert/a t.Skip("https://github.com/canonical/edgex-checkbox-provider/issues/52")' ./test/suites/edgexfoundry/proxy_test.go
 sed -i '/TestAddProxyUser/a t.Skip("https://github.com/canonical/edgex-checkbox-provider/issues/55")' ./test/suites/edgexfoundry/proxy_test.go
 
-echo "Running Go snap tests for $SUITE:"
+echo -e "\nRun the '$SUITE' test suite:"
 go test -p 1 -timeout 30m -v ./test/suites/$SUITE
+
+echo -e "\n"
