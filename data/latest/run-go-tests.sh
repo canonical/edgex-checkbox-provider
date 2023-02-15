3a9d4b8ad05b5c974f83b5b221fcf9fb3e396e95
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2023-02-15 17:59:39 +0100

    Use latest/beta as default channel (#62)
    * Use latest/beta as default channel
    
    * Set to print commands to improved debugging

--- data/latest/run-go-tests.sh
+++ data/latest/run-go-tests.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 # This scripts runs the EdgeX Snap tests maintained in
 # https://github.com/canonical/edgex-snap-testing
@@ -6,19 +6,21 @@
 # Arguments:
 SUITE=$1 # name of the Go testing package
 
-# Map input variables to those expected by the Go tests
-export PLATFORM_CHANNEL=$DEFAULT_TEST_CHANNEL
-export SERVICE_CHANNEL=$DEFAULT_TEST_CHANNEL
+DEFAULT_TEST_CHANNEL=${DEFAULT_TEST_CHANNEL:-latest/beta}
 
-# Setup the environment
-export GIT_EXEC_PATH=$SNAP/usr/lib/git-core
-export GIT_TEMPLATE_DIR=$SNAP/usr/share/git-core/templates
-export GIT_CONFIG_NOSYSTEM=1
-export PATH=$PATH:$SNAP/usr/lib/go-1.18/bin
-export CGO_ENABLED=0
+# Map input variables to those expected by the Go tests
+PLATFORM_CHANNEL=$DEFAULT_TEST_CHANNEL
+SERVICE_CHANNEL=$DEFAULT_TEST_CHANNEL
+
+GIT_EXEC_PATH=$SNAP/usr/lib/git-core
+GIT_TEMPLATE_DIR=$SNAP/usr/share/git-core/templates
+GIT_CONFIG_NOSYSTEM=1
+PATH=$PATH:$SNAP/usr/lib/go-1.18/bin
+CGO_ENABLED=0
+
+export PLATFORM_CHANNEL SERVICE_CHANNEL GIT_EXEC_PATH GIT_TEMPLATE_DIR GIT_CONFIG_NOSYSTEM PATH CGO_ENABLED
 
 rm -rf tmp
-
 git clone --config advice.detachedHead=false --depth 1 --branch v3 \
     https://github.com/canonical/edgex-snap-testing.git tmp/edgex-snap-testing
 cd tmp/edgex-snap-testing
@@ -44,5 +46,7 @@ find test -type f -exec sed -i 's/sudo //g' {} +
 sed -i '/TestTLSCert/a t.Skip("https://github.com/canonical/edgex-checkbox-provider/issues/52")' ./test/suites/edgexfoundry/proxy_test.go
 sed -i '/TestAddProxyUser/a t.Skip("https://github.com/canonical/edgex-checkbox-provider/issues/55")' ./test/suites/edgexfoundry/proxy_test.go
 
-echo "Running Go snap tests for $SUITE:"
+echo -e "\nRun the '$SUITE' test suite:"
 go test -p 1 -timeout 30m -v ./test/suites/$SUITE
+
+echo -e "\n"
